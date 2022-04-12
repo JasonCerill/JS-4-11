a8edfffbb9887f1258254d9e71bf09d1cd32658a
JasonCerill <101611107+JasonCerill@users.noreply.github.com>
2022-04-11 23:06:03 -0400

    Add files via upload

---
+++ scripts/register.js
@@ -0,0 +1,24 @@
+let petSalon = {
+    name: "The Fashion Pet",
+    address: {
+        street:  "Wagalong st",
+        zip:  "48484"
+    },
+    hours:{
+        open:"9:00am",
+        close: "8:00pm"
+    },
+    pets:[
+        {
+           // obj1
+        },
+         {
+             // obj2
+         },
+         {
+             //obj3
+         }   
+
+    ]
+}
+console.log(petSalon);
